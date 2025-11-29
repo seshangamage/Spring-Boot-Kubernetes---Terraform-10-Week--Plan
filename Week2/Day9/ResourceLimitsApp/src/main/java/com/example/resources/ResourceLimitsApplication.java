12a31ee360bf441abea6c0ef50ddf82c64b22405
Gamage <sgamage@wiley.com>
2025-11-29 19:41:55 +0530

    correcting package structure

--- Week2/Day9/ResourceLimitsApp/src/main/java/com/example/resources/ResourceLimitsApplication.java
+++ Week2/Day9/ResourceLimitsApp/src/main/java/com/example/resources/ResourceLimitsApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.example.resources;
+package com.example.resources;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
